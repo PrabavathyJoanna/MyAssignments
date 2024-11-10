5d99addfbc0b680b968ee6d226514c992e839f49
PrabavathyJoanna <praba.narmadha@gmail.com>
2024-11-10 17:14:57 +0530

    Add files via upload

---
+++ day2/FindOddNumber.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class FindOddNumber {
+	public static void main(String[] args) {
+		int maxRange=10;
+		int i;
+		for(i=0;i<maxRange;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
