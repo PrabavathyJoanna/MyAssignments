d7a9802e0e5531dedabce74cdaa1e8e595b8868e
PrabavathyJoanna <praba.narmadha@gmail.com>
2024-11-19 14:29:50 +0530

    Add files via upload
    Week2

---
+++ day1/CreateAccount.java
@@ -0,0 +1,49 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateAccount {
+
+	public static void main(String[] args) throws InterruptedException {
+	ChromeDriver driver= new ChromeDriver();
+	driver.get("http://leaftaps.com/opentaps/control/main");
+	driver.manage().window().maximize();
+	/*
+	driver.findElement(By.xpath("//input[@id='username']")).sendKeys("dilip@testleaf");
+	driver.findElement(By.xpath("//input[contains(@id,'pass')]")).sendKeys("leaf@2024");
+	driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+*/
+	driver.get("http://leaftaps.com/opentaps/control/main");
+	
+	driver.findElement(By.xpath("//input[@id='username']")).sendKeys("DemoSalesManager");
+	driver.findElement(By.xpath("(//input[@class='inputLogin'])[2]")).sendKeys("crmsfa");
+	driver.findElement(By.xpath("//input[contains(@class,'decorative')]")).click();
+	driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+	driver.findElement(By.xpath("//a[contains(text(),'Accounts')]")).click();
+	driver.findElement(By.xpath("//a[contains(text(),'Create Account')]")).click();
+	driver.findElement(By.xpath("//input[@id='accountName']")).sendKeys("Praba123");
+	driver.findElement(By.xpath("//textarea[@class='inputBox'][1]")).sendKeys("Selenium Automation Tester");
+	WebElement dropDown = driver.findElement(By.name("industryEnumId"));
+	Select opt= new Select(dropDown);
+	opt.selectByIndex(2);
+	WebElement dropDown1 = driver.findElement(By.name("ownershipEnumId"));
+	Select opt1=new Select(dropDown1);
+	opt1.selectByVisibleText("S-Corporation");
+	WebElement dropDown2 = driver.findElement(By.id("dataSourceId"));
+	Select opt2=new Select(dropDown2);
+	opt2.selectByValue("LEAD_EMPLOYEE");
+	WebElement dropDown3 = driver.findElement(By.id("marketingCampaignId"));
+	Select opt3=new Select(dropDown3);
+	opt3.selectByIndex(5);
+	WebElement dropDown4 = driver.findElement(By.id("generalStateProvinceGeoId"));
+	Select opt4=new Select(dropDown4);
+	opt4.selectByValue("TX");
+	driver.findElement(By.xpath("//input[contains(@class,'smallSubmit')]")).click();
+	Thread.sleep(5000);
+	driver.close();
+	}
+
+}
---
+++ day1/FbAccount.java
@@ -0,0 +1,55 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FbAccount {
+
+public static void main(String[] args) throws InterruptedException {
+ChromeDriver driver = new ChromeDriver();
+driver.get("https://en-gb.facebook.com/");
+driver.manage().window().maximize();
+driver.findElement(By.linkText("Create new account")).click();
+driver.findElement(By.name("firstname")).sendKeys("Prabavathy");
+driver.findElement(By.name("lastname")).sendKeys("Newton");
+WebElement dropDown = driver.findElement(By.id("day"));
+Select opt=new Select(dropDown);
+opt.selectByValue("23");
+WebElement dropDown1 = driver.findElement(By.id("month"));
+Select opt1=new Select(dropDown1);
+opt1.selectByValue("11");
+WebElement dropDown2 = driver.findElement(By.id("year"));
+Select opt2=new Select(dropDown2);
+opt2.selectByValue("1990");
+WebElement element = driver.findElement(By.xpath("//label[contains(text(),'Female')]/input"));
+element.click();
+boolean enabled = element.isSelected();
+System.out.println(enabled);
+if(enabled==false)
+{
+	element.click();
+	
+}
+driver.findElement(By.name("reg_email__")).sendKeys("praba.narmadha@gmail.com");
+driver.findElement(By.name("reg_passwd__")).sendKeys("Prathy@321");
+
+
+
+
+
+Thread.sleep(5000);
+driver.close();
+
+
+
+
+
+
+
+
+
+}
+
+}
